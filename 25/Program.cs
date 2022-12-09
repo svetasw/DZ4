22e055d74a5eb3002e738deee4d462bc99cccf6a
Sveta <svetasw_17@mail.ru>
2022-12-10 10:43:56 +1100

    25

---
+++ 25/Program.cs
@@ -0,0 +1,19 @@
+﻿int numberA = ReadInt("Enter number: ");
+int numberB = ReadInt("Enter degree: ");
+ToDegree(numberA, numberB);
+
+void ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(a + " in degree " + b + " = " + result);
+}
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
